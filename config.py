b417d5e7b8f6bb9ffa243a4890e09e96571f66de
AignerZar <zarah.aigner@student.uibk.ac.at>
2025-06-19 17:44:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,24 @@
 """
 Config file: parameters can be adjusted in this file
 """
+import torch
 
 seed = 42
-batch_size = 64
-n_epochs = 200
-latent_dimension = 2
+batch_size = 16
+n_epochs = 1000
+latent_dimension = 150
+latent_dimension_test = 2 #for testing a different set of hyperparameters
 input_dim = 60
-num_samples = 5000
+num_samples = 4949
 learning_rate = 1e-3
 validation_split = 0.2
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Path to the files
+# Path to the files -> replace for own use, the test files were used for testing different model architectures
 input_file = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/input_data.csv"
 latent_output_file = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/latent_variables.csv"
 generated_output_file = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/generated_configurations.csv"
 generated_output_denorm_file = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/generated_configurations_denorm.csv"
+generated_output_denorm_file_test = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/generated_configurations_denorm_test.csv"
+generated_output_file_test = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/generated_configurations_test.csv"
+latent_output_file_test = "/Users/zarahaigner/Documents/Physik_6 Semester/Bachelorthesis/Git-Repo/latent_variables_test.csv"
